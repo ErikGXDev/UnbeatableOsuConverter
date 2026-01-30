47a2b7f36502ca938cd0494512218ed1b31d0a14
Erik <54033728+ErikGXDev@users.noreply.github.com>
2026-01-30 17:25:22 +0100

    Initial CLI Program (unfinished)

---
+++ UnbeatableConverter.CLI/DebugProgram.cs
@@ -0,0 +1,19 @@
+﻿using UnbeatableConverter.Core;
+
+namespace UnbeatableConverter.CLI;
+
+public class DebugProgram
+{
+    public static void MainRun(string[] args)
+    {
+        Console.WriteLine("Debug Program Running...");
+
+        var inputPath = "C:\\Users\\Anwender\\Downloads\\1861042 Hinkik - Explorers.osz";
+
+        var converter = new OszExporter(inputPath);
+
+        var outputPath = converter.ExportFull();
+
+        Console.WriteLine($"Converted file saved to: {outputPath}");
+    }
+}
---
+++ UnbeatableConverter.CLI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UnbeatableConverter.CLI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        DebugProgram.MainRun(args);
+    }
+}
