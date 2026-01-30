23a204427d95e9ae45483a4b782a6c548afaf186
Erik <54033728+ErikGXDev@users.noreply.github.com>
2026-01-31 00:16:56 +0100

    Add flips to kiai time, + osu mania error

--- UnbeatableConverter.Core/Beatmap/BeatmapConverter.cs
+++ UnbeatableConverter.Core/Beatmap/BeatmapConverter.cs
@@ -1,19 +1,27 @@
 ﻿using osu.Game.Beatmaps.Formats;
 using osu.Game.IO;
+using osu.Game.Rulesets;
 using osu.Game.Rulesets.Mania;
 using osu.Game.Rulesets.Mania.Beatmaps;
+using osu.Game.Rulesets.Mania.Objects;
+using osu.Game.Rulesets.Objects;
 
 namespace UnbeatableConverter.Core.Beatmap;
 
 public class BeatmapConverter
 {
-    /// Decode the beatmap from the provided stream and convert it to a ManiaBeatmap.
+    /// Decode the beatmap from the provided stream and convert it to an UNBEATABLE-ready ManiaBeatmap.
     public ManiaBeatmap DecodeBeatmap(Stream beatmapStream)
     {
         using var osuReader = new LineBufferedReader(beatmapStream);
 
         var decoder = new LegacyBeatmapDecoder();
         var beatmap = decoder.Decode(osuReader);
+
+        if (beatmap.BeatmapInfo.Ruleset.OnlineID == 3)
+        {
+            throw new InvalidOperationException("osu!mania beatmaps are broken right now.");
+        }
 
         // Fix key, change mode
         beatmap.Difficulty.CircleSize = 5;
@@ -34,7 +42,64 @@ public class BeatmapConverter
         {
             hitObject.Samples.Clear();
 
-            hitObject.Column += 2; // Shift to center columns 3 and 4
+            // NOTE: Columns start at 0, so Column 2 and 3 are the center
+            hitObject.Column += 2; // Shift to center columns
+        }
+
+
+        // TODO: Add flips during kiai time
+        var controlPoints = convertedBeatmap.ControlPointInfo;
+        var kiaiPoints = controlPoints.EffectPoints;
+        for (var i = 0; i < kiaiPoints.Count; i++)
+        {
+            var kiaiPoint = kiaiPoints[i];
+            if (kiaiPoint.KiaiMode)
+            {
+                // Get nearest beat
+                var time = kiaiPoint.Time;
+
+                var timingPoint = controlPoints.TimingPointAt(time);
+
+                // Place zoom start
+                var zoomStart = controlPoints.GetClosestSnappedTime(time) - timingPoint.BeatLength;
+
+                var zoomStartObject = new Note()
+                {
+                    StartTime = zoomStart,
+                };
+                zoomStartObject.ApplyModifier(NoteModifier.Zoom);
+                zoomStartObject.Column = 4; // 5th Column for camera controls
+
+                // Place zoom end
+                var kiaiEndTime = (i + 1 < kiaiPoints.Count)
+                    ? kiaiPoints[i + 1].Time
+                    : beatmap.HitObjects.Last().GetEndTime();
+
+                var zoomEnd = controlPoints.GetClosestSnappedTime(kiaiEndTime) + timingPoint.BeatLength;
+                var zoomEndObject = new Note()
+                {
+                    StartTime = zoomEnd,
+                };
+                zoomEndObject.ApplyModifier(NoteModifier.Zoom);
+                zoomEndObject.Column = 4;
+
+
+                // Add flips every 4 beats during kiai
+                for (double t = time; t < kiaiEndTime; t += timingPoint.BeatLength * 4)
+                {
+                    var flipObject = new Note()
+                    {
+                        StartTime = t,
+                    };
+
+                    flipObject.Column = 4;
+
+                    convertedBeatmap.HitObjects.Add(flipObject);
+                }
+
+                convertedBeatmap.HitObjects.Add(zoomStartObject);
+                convertedBeatmap.HitObjects.Add(zoomEndObject);
+            }
         }
 
         return convertedBeatmap;
