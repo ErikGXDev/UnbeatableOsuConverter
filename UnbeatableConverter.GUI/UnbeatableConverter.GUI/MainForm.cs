5e21c2737add971f03ecf8d505967c4610bedfde
ErikGXDev <54033728+ErikGXDev@users.noreply.github.com>
2026-02-21 13:59:38 +0100

    Remove menu bar

--- UnbeatableConverter.GUI/UnbeatableConverter.GUI/MainForm.cs
+++ UnbeatableConverter.GUI/UnbeatableConverter.GUI/MainForm.cs
@@ -108,23 +108,6 @@ namespace UnbeatableConverter.GUI
                     tryBeatmaps.ExportSingleEntry(_exporter, _selectedEntry);
                 }
             };
-
-            // Quit menu
-            var quitCommand = new Command
-            {
-                MenuText = "Quit",
-                Shortcut = Application.Instance.CommonModifier | Keys.Q
-            };
-            quitCommand.Executed += (sender, e) => Application.Instance.Quit();
-
-            var aboutCommand = new Command { MenuText = "About…" };
-            aboutCommand.Executed += (sender, e) => new AboutDialog().ShowDialog(this);
-
-            Menu = new MenuBar
-            {
-                QuitItem = quitCommand,
-                AboutItem = aboutCommand
-            };
         }
     }
 }
